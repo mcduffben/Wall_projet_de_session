c4dfb9b919c1e00c756fe700154c932cfddb1442
CHERIF20 <59484497+CHERIF20@users.noreply.github.com>
2023-04-08 05:44:24 -0400

    valider

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	//ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp(new ofApp());
 
+	ofGLFWWindowSettings windowSettings;
+	windowSettings.resizable = true;
+	windowSettings.setGLVersion(3, 2);
+	windowSettings.numSamples = 4;
+	ofCreateWindow(windowSettings);
+	ofRunApp(new ofApp());
 	//COMMENTAIRE ESSAI
 
 }
