197ef0918fc2b0ba82e14b054a7d687535eb4107
mcduffben <33941564+mcduffben@users.noreply.github.com>
2023-04-09 22:07:28 -0400

    yay prise en chage de open gl marche

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,31 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
-
-	//COMMENTAIRE ESSAI
-
-}
-
-//bool use_gl_fixed_function_pipeline = false;
-////========================================================================
-//int main() {
-//    ofGLWindowSettings windowSettings;
-//    windowSettings.setSize(1024, 512);
+//int main( ){
+//	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 //
-//    if (use_gl_fixed_function_pipeline)
-//        windowSettings.setGLVersion(2, 1);
-//    else
-//        windowSettings.setGLVersion(3, 3);
+//	// can be OF_WINDOW or OF_FULLSCREEN
+//	// pass in width and height too:
+//	ofRunApp(new ofApp());
 //
-//    ofCreateWindow(windowSettings);
-//    ofRunApp(new ofApp());
-//
-//    //COMMENTAIRE ESSAI
+//	//COMMENTAIRE ESSAI
 //
 //}
+
+bool use_gl_fixed_function_pipeline = false;
+//========================================================================
+int main() {
+    ofGLWindowSettings windowSettings;
+    windowSettings.setSize(1024, 512);
+
+    if (use_gl_fixed_function_pipeline)
+        windowSettings.setGLVersion(2, 1);
+    else
+        windowSettings.setGLVersion(3, 3);
+
+    ofCreateWindow(windowSettings);
+    ofRunApp(new ofApp());
+
+    //COMMENTAIRE ESSAI
+
+}
